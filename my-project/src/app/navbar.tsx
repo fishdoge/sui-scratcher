84dba5b7a5a260aecc6c3d7362790cbd3b8b6108
aaron <ru19wu06@gmail.com>
2025-05-01 17:07:24 +0800

    update favicon Logo

--- my-project/src/app/navbar.tsx
+++ my-project/src/app/navbar.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image'
+
 import {
   SuiTransactionBlockResponse,
   SuiClient,
@@ -194,7 +196,13 @@ export default function Navbar() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-2">
-            <Sparkles className="h-6 w-6 text-purple-600" />
+            {/* <Sparkles className="h-6 w-6 text-purple-600" /> */}
+            <Image
+      src="/sui-slogo.png"
+      width={46}
+      height={46}
+      alt="Picture of the author"
+    />
             <span className="font-bold text-xl bg-gradient-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent">
               Sui Scratcher
             </span>
